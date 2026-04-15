d593ba9678104b462f1d3ad5ceaa26df307857fd
jereporopat <103338428+jereporopat@users.noreply.github.com>
2026-04-15 16:12:55 -0300

    15/04
    Tarea maquetado básico en Ángular

---
+++ tareas/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Registro } from './registro/registro';
+import { SobreMi } from './sobre-mi/sobre-mi';
+
+export const routes: Routes = [
+    {
+        path:'bienvenida',
+        loadComponent: () => import('./bienvenida/bienvenida').then((a)=>a.Bienvenida)
+    },
+    {path:'error', loadComponent:() => import ('./error/error').then((a)=>a.Error)},
+    {path:'login', loadComponent:()=> import ('./login/login').then((a)=>Login)},
+    {path:'registro', loadComponent:()=> import ('./registro/registro').then((a)=>Registro)},
+    {path:'sobre-mi', loadComponent:()=> import ('./sobre-mi/sobre-mi').then((a)=>SobreMi)},
+];
